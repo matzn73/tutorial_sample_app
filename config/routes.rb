08df1a7577fbffe562e4d535d1d896e9d193a6a9
Yuta <y.matsumo73@gmail.com>
2022-02-07 23:39:50 +0900

    add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,18 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
   root 'static_pages#home'
-
   # static_pages
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
   # users
   get '/signup', to: 'users#new'
-
   # sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
-
   # users
   resources :users
-
   resources :account_activations, only: :edit
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
